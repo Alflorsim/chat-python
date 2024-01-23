00b3936434b8c92a67521975fa912f6db9abff6d
Alfonso Lorbes <alfonsolorbes04@gmail.com>
2024-01-23 10:46:07 +0100

    hola

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 from flask_socketio import SocketIO, send
+from dotenv import load_dotenv
+import os
+# import request
+
+load_dotenv()
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'secret'
+#De esta manera (con el secret key) no se subirá la clave a github
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 socketio = SocketIO(app)
 
-
 @app.route('/')
+
 def index():
     return render_template('index.html')
 
 @socketio.on('message')
+
 def handleMessage(msg):
-    print("Message:", msg)
+    print("Message: ",msg)
     send(msg, broadcast = True)
 
-    clientIP = request
-    print(clientIP)
+    # clientIP = request.connection.remoteAddresss
+    # print(clientIP)
 
-if __name__=='__main__':
-
-    socketio.run(app, port=3000, debug=True, host="0.0.0.0")
-
-
+if __name__ == "__main__":
+    socketio.run(app, port=3000, debug=True) #Si añadimos al final host="0.0.0.0" y alguien pone nuestra IP en su navegador, será un chat entre los 2
